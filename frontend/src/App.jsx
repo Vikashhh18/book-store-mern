65b26de69d2b6c445eb41d8c655e00d6000e1b7e
Vikashhh18 <Vikash.sharma1761@gmail.com>
2025-06-26 16:06:50 +0530

    nabar to Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import Navbar from './components/navbar'
 import Footer from './components/footer'
 import { AuthProvider } from './context/authContext'
+import Navbar from './components/Navbar'
+
 
 const App = () => {
   return (
